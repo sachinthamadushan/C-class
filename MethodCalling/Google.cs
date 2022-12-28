171bf99bcfed2e682155c6440ecd1974165e23c9
Sachintha <sachimadu68@gmail.com>
2022-12-28 12:15:21 +0530

    Create google.cs file

---
+++ MethodCalling/Google.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoogleLand{
+
+    class Google
+    {
+        static void Main(string[] args){
+            System.Console.WriteLine("Welcome to Google");
+        }
+    }
+}
