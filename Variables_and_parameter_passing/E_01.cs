dff11eb44589e152777e6ef1bbaa32c7d2a905d2
Sachintha <sachimadu68@gmail.com>
2022-12-29 19:38:55 +0530

    Add new folder & c# file

---
+++ Variables_and_parameter_passing/E_01.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Apple
+{
+    class Iphone
+    {
+        static string model = "14 Pro Max";
+        static double price = "250000";
+        
+        static void Main(string[] args){
+            System.Console.WriteLine(model);
+            System.Console.WriteLine("Price:"+price);
+        }
+    }
+}
