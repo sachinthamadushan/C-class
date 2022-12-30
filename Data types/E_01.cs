fb1e390be5d608a2edff91b3a4c70c3dd9853e29
unknown <sachimadu68@gmail.com>
2022-12-30 14:18:08 +0530

    add new file

---
+++ Data types/E_01.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Apple_1
+{
+    internal class NewBaseType
+    {
+        
+    }
+
+    class Iphone : NewBaseType
+    {
+      static void Main(string[] args) {
+        
+      }
+    }
+}
