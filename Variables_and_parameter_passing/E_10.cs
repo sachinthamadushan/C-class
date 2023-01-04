d46dccf9769341a5061f64cd577562965cc69ad5
Sachintha <sachimadu68@gmail.com>
2023-01-04 07:36:26 +0530

    Updated

--- Variables_and_parameter_passing/E_10.cs
+++ Variables_and_parameter_passing/E_10.cs
@@ -1,3 +1,4 @@
+using System;
 namespace Cal
 {
     class SC
@@ -5,9 +6,11 @@ namespace Cal
         static void Main(string[] a){
             System.Console.WriteLine("Enter 1st number >>");
             string fn = System.Console.ReadLine();
+            int x  = int.Parse(fn);
             System.Console.WriteLine("Enter 1st number >>");
             string sn = System.Console.ReadLine();
-            string tot = fn+sn;
+            int y  = int.Parse(sn);
+            int tot = x+y;
             System.Console.WriteLine("Result = "+tot);
         }
     }
